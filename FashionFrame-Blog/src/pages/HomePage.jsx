0b722886d6a83ec09b239598ce2bc417b91e4a81
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-02 10:24:33 -0600

    feat: added more components to HomePage to see CSS styling effect into HomePage

--- FashionFrame-Blog/src/pages/HomePage.jsx
+++ FashionFrame-Blog/src/pages/HomePage.jsx
@@ -3,19 +3,27 @@ import VolumeControl from '../components/VolumeControl/VolumeControl';
 import MusicPlayer from '../components/MusicPlayer/MusicPlayer';
 import LatestUpdate from '../components/LatestUpdate/LatestUpdate';
 import GifDisplay from '../components/GifDisplay/GifDisplay';
+import UserStatus from '../components/UserStatus/UserStatus';
+import '../styles/HomePage.css';
 import { useState } from 'react';
 
 const HomePage = () => {
   const [volume, setVolume] = useState(50);
 
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is your home page after login.</p>
-      <VolumeControl volume={volume} setVolume={setVolume} />
-      <MusicPlayer volume={volume} />
-      <LatestUpdate />
-      <GifDisplay />
+    <div className="main-container">
+      <div className="section start-section">
+        <VolumeControl volume={volume} setVolume={setVolume} />
+        <MusicPlayer volume={volume} />
+        <LatestUpdate />
+        <GifDisplay />
+      </div>
+      <div className="section main-section">
+        <MusicPlayer volume={volume}></MusicPlayer>
+      </div>
+      <div className="section end-section">
+        <UserStatus username="JohnDoe" onLogout={() => {}} />
+      </div>
     </div>
   );
 };
