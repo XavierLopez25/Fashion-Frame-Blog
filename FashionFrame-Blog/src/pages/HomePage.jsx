bc046135d8999d1f20ac43ef6de65539a0c29b9c
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-01 18:59:18 -0600

    feat: added useState for volume in MusicPlayer

--- FashionFrame-Blog/src/pages/HomePage.jsx
+++ FashionFrame-Blog/src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import VolumeControl from '../components/VolumeControl/VolumeControl';
+import MusicPlayer from '../components/MusicPlayer/MusicPlayer';
+import { useState } from 'react';
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is your home page after login.</p>
-        </div>
-    );
+  const [volume, setVolume] = useState(50);
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is your home page after login.</p>
+      <VolumeControl volume={volume} setVolume={setVolume} />
+      <MusicPlayer volume={volume} />
+    </div>
+  );
 };
 
 export default HomePage;
