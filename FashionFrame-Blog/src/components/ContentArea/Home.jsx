5a8eaf532ef5397c9c5e01ad189bb62ce6e7899c
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-03 18:15:51 -0600

    feat: added Home component for ContentArea

---
+++ FashionFrame-Blog/src/components/ContentArea/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="image-bannerH"></div>
+      <div className="welcome-message">Welcome to the blog!</div>
+      <div className="purpose-section">
+        <p>
+          Welcome to the ultimate hub for all Warframe Fashion Frame enthusiasts! Whether you’re a
+          seasoned Tenno or just starting your journey, this is the place to showcase your most
+          stunning Warframe designs. <br />
+          <br />
+          Dive into a universe where creativity meets combat, and style is just as important as
+          strength. Share your unique Fashion Frames, discover new inspirations, and connect with
+          fellow fashion warriors. From sleek and sophisticated to wild and wondrous, we celebrate
+          all styles here. Start posting your looks today and let the galaxy admire your artistic
+          flair!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
