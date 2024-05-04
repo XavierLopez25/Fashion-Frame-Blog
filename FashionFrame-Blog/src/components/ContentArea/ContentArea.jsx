1ef19d78f55e26366c9a7d4c1711e63cf95ae6a2
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-03 18:18:07 -0600

    feat: added Home, AboutMe and WhereToFindMe components to ContentArea component

--- FashionFrame-Blog/src/components/ContentArea/ContentArea.jsx
+++ FashionFrame-Blog/src/components/ContentArea/ContentArea.jsx
@@ -1,35 +1,20 @@
 import React, { useState } from 'react';
 import '../../styles/ContentArea.css';
+import Home from './Home';
+import AboutMe from './AboutMe';
+import WhereToFindMe from './WhereToFindMe';
 
 const Main = ({ currentSection }) => {
   const renderSection = () => {
     switch (currentSection) {
       case 'HOME':
-        return (
-          <div className="home-container">
-            <div className="image-bannerH"></div>
-            <div className="welcome-message">Welcome to the blog!</div>
-            <div className="purpose-section">
-              <p>
-                Welcome to the ultimate hub for all Warframe Fashion Frame enthusiasts! Whether
-                you’re a seasoned Tenno or just starting your journey, this is the place to showcase
-                your most stunning Warframe designs. <br />
-                <br />
-                Dive into a universe where creativity meets combat, and style is just as important
-                as strength. Share your unique Fashion Frames, discover new inspirations, and
-                connect with fellow fashion warriors. From sleek and sophisticated to wild and
-                wondrous, we celebrate all styles here. Start posting your looks today and let the
-                galaxy admire your artistic flair!
-              </p>
-            </div>
-          </div>
-        );
+        return <Home />;
       case 'POSTS':
         return <div>Here are the posts.</div>;
       case 'ABOUT ME':
-        return <div>About the author...</div>;
+        return <AboutMe />;
       case 'WHERE TO FIND ME':
-        return <div>Contact details and social media links.</div>;
+        return <WhereToFindMe />;
       default:
         return <div>Content not found!</div>;
     }
