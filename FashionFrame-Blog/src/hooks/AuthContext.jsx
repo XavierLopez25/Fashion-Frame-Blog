abf33e6ac441c57b6f06596c4b77447bcb28c2ef
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-05 17:10:47 -0600

    feat: added User Authentication hook

---
+++ FashionFrame-Blog/src/hooks/AuthContext.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [authToken, setAuthToken] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userDataJson = localStorage.getItem('user');
+
+    if (token) {
+      setAuthToken(token);
+    }
+    if (userDataJson) {
+      try {
+        const userData = JSON.parse(userDataJson);
+        setUser(userData);
+      } catch (error) {}
+    }
+  }, []);
+
+  const login = (token, userData) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(userData));
+    setAuthToken(token);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setAuthToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ authToken, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
