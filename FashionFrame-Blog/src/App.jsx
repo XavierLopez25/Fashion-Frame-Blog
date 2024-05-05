d875ddc16fd0bb20a13a2f189537975dbb8a6b17
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-05 17:11:12 -0600

    feat: added Authorization Provider

--- FashionFrame-Blog/src/App.jsx
+++ FashionFrame-Blog/src/App.jsx
@@ -2,16 +2,20 @@ import './styles/App.css';
 import { LoginRegister } from './components/LoginRegister/LoginRegister';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/AuthContext';
+
 import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginRegister />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginRegister />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
