080ffaf82ce72c04dcb796f4b5c48956162f71a7
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-05 17:10:05 -0600

    feat: added SuccessMessage component

---
+++ FashionFrame-Blog/src/components/SuccessMessage/SuccessMessage.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import '../../styles/SuccessMessage.css';
+
+const SuccessMessage = ({ message, duration = 3000, onClose }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      if (onClose) onClose(); // Ejecutar cualquier función adicional al cerrar
+    }, duration);
+
+    return () => clearTimeout(timer); // Limpieza al desmontar
+  }, [duration, onClose]);
+
+  if (!visible) return null;
+
+  return (
+    <div className="success-message" style={{ animation: `fadeInOut ${duration}ms` }}>
+      {message}
+    </div>
+  );
+};
+
+export default SuccessMessage;
