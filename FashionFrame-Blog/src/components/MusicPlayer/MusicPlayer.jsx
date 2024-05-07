bf67216f224b928ef10a13691dc2b69dc7a0fca6
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-07 09:14:56 -0600

    fix: fixed Forward and Backward functions

--- FashionFrame-Blog/src/components/MusicPlayer/MusicPlayer.jsx
+++ FashionFrame-Blog/src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,121 +1,121 @@
-import React, { useState, useRef, useEffect } from 'react'
-import '../../styles/MusicPlayer.css'
-import CustomDropdown from '../SongSelector/SongSelector.jsx'
-import { VscChromeMaximize, VscChromeMinimize, VscChromeClose } from 'react-icons/vsc'
-import { LiaVolumeUpSolid } from 'react-icons/lia'
-import { MdFastForward, MdFastRewind, MdOutlinePause, MdLoop } from 'react-icons/md'
-import { IoIosPlay } from 'react-icons/io'
+import React, { useState, useRef, useEffect } from 'react';
+import '../../styles/MusicPlayer.css';
+import CustomDropdown from '../SongSelector/SongSelector.jsx';
+import { VscChromeMaximize, VscChromeMinimize, VscChromeClose } from 'react-icons/vsc';
+import { LiaVolumeUpSolid } from 'react-icons/lia';
+import { MdFastForward, MdFastRewind, MdOutlinePause, MdLoop } from 'react-icons/md';
+import { IoIosPlay } from 'react-icons/io';
 
 const MusicPlayer = ({ volume }) => {
-  const [currentSong, setCurrentSong] = useState('')
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [currentSongName, setCurrentSongName] = useState('Select a Song')
-  const [isLooping, setIsLooping] = useState(false)
-  const audioRef = useRef(new Audio())
+  const [currentSong, setCurrentSong] = useState('');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSongName, setCurrentSongName] = useState('Select a Song');
+  const [isLooping, setIsLooping] = useState(false);
+  const audioRef = useRef(new Audio());
 
   const songs = [
     {
       id: 1,
       name: 'Cream Blade, romi - Daydream',
-      url: '../../assets/MP3/Cream Blade, romi - Daydream.mp3'
+      url: '../../assets/MP3/Cream Blade, romi - Daydream.mp3',
     },
     { id: 2, name: 'Dawn Wall - Spears', url: '../../assets/MP3/Dawn Wall - Spears.mp3' },
     {
       id: 3,
       name: 'Droptek - Back 2 U',
-      url: '../../assets/MP3/Droptek - Back 2 U.mp3'
+      url: '../../assets/MP3/Droptek - Back 2 U.mp3',
     },
     {
       id: 4,
       name: 'Droptek, Isabel Higuero - Killing Time',
-      url: '../../assets/MP3/Droptek, Isabel Higuero - Killing Time.mp3'
+      url: '../../assets/MP3/Droptek, Isabel Higuero - Killing Time.mp3',
     },
     { id: 5, name: 'Fox Stevenson - Knowhow', url: '../../assets/MP3/Fox Stevenson - Knowhow.mp3' },
     { id: 6, name: 'JNR.h - Waking Up', url: '../../assets/MP3/JNR.h - Waking Up.mp3' },
     {
       id: 7,
       name: 'Lenzman, Artificial Intelligence - String City - Artificial Intelligence Remix',
-      url: '../../assets/MP3/Lenzman, Artificial Intelligence - String City - Artificial Intelligence Remix.mp3'
+      url: '../../assets/MP3/Lenzman, Artificial Intelligence - String City - Artificial Intelligence Remix.mp3',
     },
     { id: 8, name: 'Monroe - Eventide', url: '../../assets/MP3/Monroe - Eventide.mp3' },
     {
       id: 9,
       name: 'Peter Lix - Echoes Of My Mind',
-      url: '../../assets/MP3/Peter Lix - Echoes Of My Mind.mp3'
+      url: '../../assets/MP3/Peter Lix - Echoes Of My Mind.mp3',
     },
     {
       id: 10,
       name: 'Polaris - Prismatic Gate',
-      url: '../../assets/MP3/Polaris - Prismatic Gate.mp3'
-    }
-  ]
+      url: '../../assets/MP3/Polaris - Prismatic Gate.mp3',
+    },
+  ];
 
   useEffect(() => {
-    audioRef.current.volume = volume / 100
-    audioRef.current.loop = isLooping
-  }, [volume, isLooping])
+    audioRef.current.volume = volume / 100;
+    audioRef.current.loop = isLooping;
+  }, [volume, isLooping]);
 
   useEffect(() => {
     if (currentSong) {
-      audioRef.current.src = currentSong
-      setIsPlaying(true)
+      audioRef.current.src = currentSong;
+      setIsPlaying(true);
     }
-  }, [currentSong])
+  }, [currentSong]);
 
   useEffect(() => {
-    audioRef.current.loop = isLooping
-  }, [isLooping])
+    audioRef.current.loop = isLooping;
+  }, [isLooping]);
 
   useEffect(() => {
     if (isPlaying) {
-      audioRef.current.play()
+      audioRef.current.play();
     } else {
-      audioRef.current.pause()
+      audioRef.current.pause();
     }
-  }, [isPlaying])
+  }, [isPlaying]);
 
   const handleSongChange = (song) => {
-    setCurrentSong(song.url)
-    setCurrentSongName(song.name)
-    setIsPlaying(true)
-  }
+    setCurrentSong(song.url);
+    setCurrentSongName(song.name);
+    setIsPlaying(true);
+  };
 
   const togglePlay = () => {
-    setIsPlaying(!isPlaying)
-  }
+    setIsPlaying(!isPlaying);
+  };
 
   const toggleLoop = () => {
-    setIsLooping(!isLooping)
-  }
+    setIsLooping(!isLooping);
+  };
 
   const handleNext = () => {
     const currentIndex = songs.findIndex((song) => {
-      return song.url === currentSong
-    })
-    const nextIndex = (currentIndex + 1) % songs.length
-    handleSongChange(songs[nextIndex])
-  }
+      return song.url === currentSong;
+    });
+    const nextIndex = (currentIndex + 1) % songs.length;
+    handleSongChange(songs[nextIndex]);
+  };
 
   const handlePrevious = () => {
-    const currentIndex = songs.findIndex((song) => song.url === currentSong)
-    const previousIndex = (currentIndex - 1 + songs.length) % songs.length
-    handleSongChange(songs[previousIndex])
-  }
+    const currentIndex = songs.findIndex((song) => song.url === currentSong);
+    const previousIndex = (currentIndex - 1 + songs.length) % songs.length;
+    handleSongChange(songs[previousIndex]);
+  };
 
   const onAudioEnded = () => {
     if (!isLooping) {
-      handleNext()
+      handleNext();
     }
-  }
+  };
 
   useEffect(() => {
-    const audio = audioRef.current
-    audio.addEventListener('ended', onAudioEnded)
+    const audio = audioRef.current;
+    audio.addEventListener('ended', onAudioEnded);
 
     return () => {
-      audio.removeEventListener('ended', onAudioEnded)
-    }
-  }, [isLooping, handleNext, currentSong])
+      audio.removeEventListener('ended', onAudioEnded);
+    };
+  }, [isLooping, handleNext, currentSong]);
 
   return (
     <div className="music-player">
@@ -174,7 +174,7 @@ const MusicPlayer = ({ volume }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MusicPlayer
+export default MusicPlayer;
