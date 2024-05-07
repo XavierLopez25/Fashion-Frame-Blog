87c4b26a70507d4dc89e7a4698f39ac5248b59df
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-07 09:17:37 -0600

    feat: added management for states in Main compoent

--- FashionFrame-Blog/src/components/ContentArea/ContentArea.jsx
+++ FashionFrame-Blog/src/components/ContentArea/ContentArea.jsx
@@ -1,27 +1,40 @@
-import React, { useState } from 'react'
-import '../../styles/ContentArea.css'
-import Home from './Home'
-import AboutMe from './AboutMe'
-import WhereToFindMe from './WhereToFindMe'
-import Posts from './Posts'
+import React, { useState } from 'react';
+import '../../styles/ContentArea.css';
+import Home from './Home';
+import AboutMe from './AboutMe';
+import WhereToFindMe from './WhereToFindMe';
+import Posts from './Posts';
+import { ErrorState, Loading, Empty } from '../Status/Status';
 
-const Main = ({ currentSection, posts }) => {
+const Main = ({ currentSection, posts, isLoading, error }) => {
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <ErrorState message={error} />;
+  }
+
+  if (posts.length === 0 && currentSection === 'POSTS') {
+    return <Empty />;
+  }
+
   const renderSection = () => {
     switch (currentSection) {
       case 'HOME':
-        return <Home />
+        return <Home />;
       case 'POSTS':
-        return <Posts posts={posts}></Posts>
+        return <Posts posts={posts}></Posts>;
       case 'ABOUT ME':
-        return <AboutMe />
+        return <AboutMe />;
       case 'WHERE TO FIND ME':
-        return <WhereToFindMe />
+        return <WhereToFindMe />;
       default:
-        return <div>Content not found!</div>
+        return <div>Content not found!</div>;
     }
-  }
+  };
 
-  return <div className="main-content">{renderSection()}</div>
-}
+  return <div className="main-content">{renderSection()}</div>;
+};
 
-export default Main
+export default Main;
